648203d77c8a97cf2a018ddaff7d6c7ed4cec426
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2026-09-14 19:09:31 +0600

    Update vite.config.js, modules_statuses.json

--- Modules/EMS/vite.config.js
+++ Modules/EMS/vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+export const paths = [
+    "Modules/EMS/resources/assets/sass/app.scss",
+    "Modules/EMS/resources/assets/js/app.js",
+];
 
 export default defineConfig({
     build: {
-        outDir: '../../public/build-ems',
+        outDir: "../../public/build-ems",
         emptyOutDir: true,
         manifest: true,
     },
     plugins: [
         laravel({
-            publicDirectory: '../../public',
-            buildDirectory: 'build-ems',
+            publicDirectory: "../../public",
+            buildDirectory: "build-ems",
             input: [
-                __dirname + '/resources/assets/js/app.js',
-                __dirname + '/resources/assets/sass/app.scss'
+                __dirname + "/resources/assets/js/app.js",
+                __dirname + "/resources/assets/sass/app.scss",
             ],
             refresh: true,
         }),
